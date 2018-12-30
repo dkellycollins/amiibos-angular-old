1ebff30a8b711c291c7d24b90250b092e9672a03
Devin Kelly-Collins <dkellycollins@gmail.com>
2018-12-30 12:53:37 -0600

    Implemented amiibos-table.

--- src/app/amiibos/amiibos-table/amiibos-table.component.ts
+++ src/app/amiibos/amiibos-table/amiibos-table.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-amiibos-table',
-  templateUrl: './amiibos-table.component.html',
-  styleUrls: ['./amiibos-table.component.scss']
+  templateUrl: './amiibos-table.component.html'
 })
 export class AmiibosTableComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['name', 'series', 'releaseDate'];
+  dataSource = undefined;
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.dataSource = this.http.get('https://amiibos-elm.herokuapp.com/api/amiibos');
   }
 
 }
--- src/app/amiibos/amiibos.module.ts
+++ src/app/amiibos/amiibos.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AmiibosTableComponent } from './amiibos-table/amiibos-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AmiibosTableComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    MatTableModule
   ]
 })
 export class AmiibosModule { }
