70833e28931d94256bcb4db078bb489a62312e40
Devin Kelly-Collins <dkellycollins@gmail.com>
2018-12-30 16:03:06 -0600

    Added side navigation.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 import { StandardLayoutComponent } from './standard-layout/standard-layout.component';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [StandardLayoutComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([]),
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class CoreModule { }
